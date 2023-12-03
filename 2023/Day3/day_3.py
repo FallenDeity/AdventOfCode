c7a39abdedc6dea84cbe7c19c3a1ffae75880d60
FallenDeity <61227305+FallenDeity@users.noreply.github.com>
2023-12-03 12:58:29 +0530

    feat: Day 3

--- 2023/Day3/day_3.py
+++ 2023/Day3/day_3.py
@@ -1,4 +1,43 @@
+from __future__ import annotations
+
+import dataclasses
 import typing
+
+SYMBOLS = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~".replace(".", "")
+
+
+@dataclasses.dataclass
+class Cell:
+    value: str
+    row: int
+    span: tuple[int, int]
+    is_symbol: bool = False
+
+    def __int__(self) -> int:
+        return int(self.value) if not self.is_symbol else 0
+
+    def is_adjacent(self, cell: Cell) -> bool:
+        return (
+            cell is not self
+            and cell.row in range(self.row - 1, self.row + 2)
+            and self.span[0] in range(cell.span[0] - 1, cell.span[1] + 1)
+        )
+
+
+def parse_cells(data: list[str]) -> list[Cell]:
+    cells: list[Cell] = []
+    for y, row in enumerate(data):
+        values = [Cell(m, y, (n, n + 1), True) for n, m in enumerate(row) if m in SYMBOLS]
+        for n, m in enumerate(row):
+            if m.isdigit() and not (n > 0 and row[n - 1].isdigit()):
+                end = [i for i, c in enumerate(row[n:]) if not c.isdigit()] or [len(row) - n]
+                values.append(Cell(row[n : n + end[0]], y, (n, n + end[0])))
+        cells.extend(values)
+    return cells
+
+
+def get_adjacent(cell: Cell, cells: list[Cell]) -> list[Cell]:
+    return [c for c in cells if cell.is_adjacent(c)]
 
 
 def get_input(file_name: str) -> typing.List[str]:
@@ -7,14 +46,15 @@ def get_input(file_name: str) -> typing.List[str]:
     return lines
 
 
-def part_a(lines: typing.List[str]) -> int:
-    return 0
+def part_a(cells: list[Cell]) -> int:
+    return sum(sum([[int(c) for c in get_adjacent(c, cells)] for c in cells if c.is_symbol], []))
 
 
-def part_b(lines: typing.List[str]) -> int:
-    return 0
+def part_b(cells: list[Cell]) -> int:
+    return sum([int(g[0]) * int(g[1]) for c in cells if c.value == "*" and len((g := get_adjacent(c, cells))) == 2])
 
 
 if __name__ == "__main__":
-    print(part_a(get_input("../bin/3.txt")))
-    print(part_b(get_input("../bin/3.txt")))
+    cells = parse_cells(get_input("../bin/3.txt"))
+    print(part_a(cells))
+    print(part_b(cells))
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - 2023
     - [Day 1](2023/Day1/day_1.py)
     - [Day 2](2023/Day2/day_2.py)
+    - [Day 3](2023/Day3/day_3.py)
