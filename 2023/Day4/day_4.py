4886b00b247b7e90b11e0ef25d46ebbe88d5ed04
FallenDeity <61227305+FallenDeity@users.noreply.github.com>
2023-12-04 13:16:25 +0530

    feat: Day 4

--- 2023/Day4/day_4.py
+++ 2023/Day4/day_4.py
@@ -1,4 +1,29 @@
+from __future__ import annotations
+
+import dataclasses
 import typing
+
+
+@dataclasses.dataclass
+class Card:
+    id: int
+    numbers: typing.List[int]
+    winners: typing.List[int]
+    value: int = 0
+
+    def __post_init__(self) -> None:
+        self.value = 2 ** (len(self.common) - 1) if self.common else 0
+
+    @property
+    def common(self) -> typing.Set[int]:
+        return set(self.numbers).intersection(self.winners)
+
+    @classmethod
+    def parse_card(cls, line: str) -> Card:
+        header, numbers = line.split(":")
+        id = int(header.split(" ")[-1])
+        winners, numbers = numbers.split("|")
+        return cls(id, [int(n) for n in numbers.split()], [int(n) for n in winners.split()])
 
 
 def get_input(file_name: str) -> typing.List[str]:
@@ -8,11 +33,16 @@ def get_input(file_name: str) -> typing.List[str]:
 
 
 def part_a(lines: typing.List[str]) -> int:
-    return 0
+    return sum([Card.parse_card(line).value for line in lines])
 
 
 def part_b(lines: typing.List[str]) -> int:
-    return 0
+    cards = {c.id: [c] for c in [Card.parse_card(line) for line in lines]}
+    for k, v in cards.items():
+        copies = {c: cards[c][0] for i in range(k + 1, len(v[0].common) + 1 + k) for c in cards if c == i}
+        for i, j in copies.items():
+            cards[i].extend([j] * len(cards[k]))
+    return sum([len(v) for v in cards.values()])
 
 
 if __name__ == "__main__":
