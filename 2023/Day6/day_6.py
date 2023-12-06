61a62ccd9e9b97db4666493d4803a3f26ec78ba2
FallenDeity <61227305+FallenDeity@users.noreply.github.com>
2023-12-06 14:23:00 +0530

    fix: fix test case bug in day 6 `solve_d`

--- 2023/Day6/day_6.py
+++ 2023/Day6/day_6.py
@@ -9,12 +9,13 @@ def get_input(file_name: str) -> typing.List[str]:
 
 
 def solve_d(t: float, d: float) -> float:
-    return float((t**2 - 4 * d) ** 0.5)
+    return float(v - 1 if (v := (t**2 - 4 * d) ** 0.5) % 1 == 0 else v)
 
 
 def part_a(lines: typing.List[str]) -> int:
     data = dict(zip(*[map(int, i.split(":")[-1].split()) for i in lines]))
     values = [((k + solve_d(k, v)) // 2) - ((k - solve_d(k, v)) // 2) for k, v in data.items()]
+    print(values)
     return int(prod(values))
 
 
