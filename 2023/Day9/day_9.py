ff59a33184e7ae01b5e03ef8501983dd26b77757
FallenDeity <61227305+FallenDeity@users.noreply.github.com>
2023-12-09 13:03:52 +0530

    feat: Day 9

--- 2023/Day9/day_9.py
+++ 2023/Day9/day_9.py
@@ -7,12 +7,19 @@ def get_input(file_name: str) -> typing.List[str]:
     return lines
 
 
+def extrapolate(nums: typing.List[int]) -> int:
+    nn = [b - a for a, b in zip(nums, nums[1:])]
+    return nums[-1] + extrapolate(nn) if any(nn) else nums[-1]
+
+
 def part_a(lines: typing.List[str]) -> int:
-    return 0
+    nums = [list(map(int, line.split())) for line in lines]
+    return sum(extrapolate(num) for num in nums)
 
 
 def part_b(lines: typing.List[str]) -> int:
-    return 0
+    nums = [list(map(int, line.split())) for line in lines]
+    return sum(extrapolate(num[::-1]) for num in nums)
 
 
 if __name__ == "__main__":
