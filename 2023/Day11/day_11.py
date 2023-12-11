3e1e2e127446be7987caeadedcc0e4e0f3535f46
FallenDeity <61227305+FallenDeity@users.noreply.github.com>
2023-12-11 19:41:50 +0530

    feat: Day 11

--- 2023/Day11/day_11.py
+++ 2023/Day11/day_11.py
@@ -7,12 +7,23 @@ def get_input(file_name: str) -> typing.List[str]:
     return lines
 
 
+def expand(universe: typing.List[str], factor: float) -> int:
+    row_distance, total_distance = 0.0, 0.0
+    seen_galaxies, space = 0.0, 0.0
+    for count in map(lambda x: x.count("#"), universe):
+        row_distance += seen_galaxies * space
+        total_distance += row_distance * count
+        seen_galaxies += count
+        space = 1 if count else factor
+    return int(total_distance)
+
+
 def part_a(lines: typing.List[str]) -> int:
-    return 0
+    return expand(lines, 2) + expand(["".join(i) for i in zip(*lines)], 2)
 
 
 def part_b(lines: typing.List[str]) -> int:
-    return 0
+    return expand(lines, 1e6) + expand(["".join(i) for i in zip(*lines)], 1e6)
 
 
 if __name__ == "__main__":
