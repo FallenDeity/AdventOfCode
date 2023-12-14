cb7672c567ffe34d4290dc588fb88a745172231f
FallenDeity <61227305+FallenDeity@users.noreply.github.com>
2023-12-14 12:44:44 +0530

    feat: Day 14

--- 2023/Day13/day_13.py
+++ 2023/Day13/day_13.py
@@ -1,18 +1,31 @@
 import typing
 
 
-def get_input(file_name: str) -> typing.List[str]:
+def get_input(file_name: str) -> typing.List[typing.List[str]]:
     with open(file_name, "r") as f:
-        lines = f.read().splitlines()
-    return lines
+        lines = f.read().split("\n\n")
+    return [line.splitlines() for line in lines]
 
 
-def part_a(lines: typing.List[str]) -> int:
-    return 0
+def total(case: typing.List[str], idx: int) -> int:
+    return sum(sum(int(a != b) for a, b in zip(x, y)) for x, y in zip(case[idx:], case[idx - 1 :: -1]))
 
 
-def part_b(lines: typing.List[str]) -> int:
-    return 0
+def find_mirror(case: typing.List[str], smudges: int = 0) -> int:
+    case_t = list(map("".join, zip(*case)))
+    for m, c in ((100, case), (1, case_t)):
+        for i in range(1, len(c)):
+            if total(c, i) == smudges:
+                return m * i
+    raise ValueError
+
+
+def part_a(lines: typing.List[typing.List[str]]) -> int:
+    return sum(find_mirror(case) for case in lines)
+
+
+def part_b(lines: typing.List[typing.List[str]]) -> int:
+    return sum(find_mirror(case, 1) for case in lines)
 
 
 if __name__ == "__main__":
--- 2023/Day14/day_14.py
+++ 2023/Day14/day_14.py
@@ -1,4 +1,8 @@
+import functools
 import typing
+from itertools import count
+
+ROW = typing.Tuple[str, ...]
 
 
 def get_input(file_name: str) -> typing.List[str]:
@@ -7,14 +11,57 @@ def get_input(file_name: str) -> typing.List[str]:
     return lines
 
 
-def part_a(lines: typing.List[str]) -> int:
-    return 0
+@functools.cache
+def rotate(lines: typing.Tuple[ROW, ...]) -> typing.Tuple[ROW, ...]:
+    return tuple(x[::-1] for x in zip(*lines))
 
 
-def part_b(lines: typing.List[str]) -> int:
-    return 0
+@functools.cache
+def fall(lines: typing.Tuple[ROW, ...]) -> typing.Tuple[ROW, ...]:
+    new_lines = []
+    for col in zip(*lines):
+        new_col = ["."] * len(col)
+        new_lines.append(new_col)
+        i = 0
+        for y, c in enumerate(col, start=1):
+            if c == "#":
+                new_col[y - 1] = c
+                i = y
+            if c == "O":
+                new_col[i] = c
+                i += 1
+    return tuple(zip(*new_lines))
+
+
+@functools.cache
+def cycle_lines(lines: typing.Tuple[ROW, ...]) -> typing.Tuple[ROW, ...]:
+    for _ in range(4):
+        lines = rotate(fall(lines))
+    return lines
+
+
+def calculate_load(lines: typing.Tuple[ROW, ...]) -> int:
+    return sum(y * row.count("O") for y, row in enumerate(reversed(lines), 1))
+
+
+def part_a(lines: typing.Tuple[ROW, ...]) -> int:
+    return calculate_load(fall(lines))
+
+
+def part_b(lines: typing.Tuple[ROW, ...]) -> int:
+    tortoise = hare = lines
+    for i in count(1):
+        tortoise = cycle_lines(tortoise)
+        hare = cycle_lines(cycle_lines(hare))
+        if tortoise == hare:
+            break
+    cycle_length, initial = i, 2 * i
+    n = 1000000000
+    for _ in range((n - initial) % cycle_length):
+        tortoise = cycle_lines(tortoise)
+    return calculate_load(tortoise)
 
 
 if __name__ == "__main__":
-    print(part_a(get_input("../bin/14.txt")))
-    print(part_b(get_input("../bin/14.txt")))
+    print(part_a(tuple(map(tuple, get_input("../bin/14.txt")))))
+    print(part_b(tuple(map(tuple, get_input("../bin/14.txt")))))
