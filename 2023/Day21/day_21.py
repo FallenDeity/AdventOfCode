65eacb9379841b3dad3c9904917cae506951e272
FallenDeity <61227305+FallenDeity@users.noreply.github.com>
2023-12-21 12:31:28 +0530

    feat: Day 21

--- 2023/Day21/day_21.py
+++ 2023/Day21/day_21.py
@@ -1,4 +1,5 @@
 import typing
+from collections import deque
 
 
 def get_input(file_name: str) -> typing.List[str]:
@@ -7,12 +8,32 @@ def get_input(file_name: str) -> typing.List[str]:
     return lines
 
 
-def part_a(lines: typing.List[str]) -> int:
-    return 0
+def part_a(garden: typing.List[str], steps: int = 64) -> int:
+    x_l, y_l = len(garden), len(garden[0])
+    r, c = [(i, j) for i in range(len(garden)) for j in range(len(garden[0])) if garden[i][j] == "S"][0]
+    queue = deque([(r, c, 0)])
+    visited = set()
+    step_map: typing.Dict[int, typing.Set[typing.Tuple[int, int]]] = {}
+    while queue:
+        row, col, step = queue.popleft()
+        if (row, col) not in visited and step <= steps:
+            visited.add((row, col))
+            for direction in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                new_row, new_col = row + direction[0], col + direction[1]
+                if garden[new_row % x_l][new_col % y_l] != "#":
+                    queue.append((new_row, new_col, step + 1))
+                    visited.discard((new_row, new_col))
+                    step_map.setdefault(step + 1, set()).add((new_row, new_col))
+    return len(step_map[steps])
 
 
-def part_b(lines: typing.List[str]) -> int:
-    return 0
+def part_b(garden: typing.List[str], steps: int = 26501365) -> int:
+    a0 = 3848  # part_a(garden, 65)
+    a1 = 34310  # part_a(garden, 65 + 131)
+    a2 = 95144  # part_a(garden, 65 + 131 * 2)
+    b0, b1, b2 = a0, a1 - a0, a2 - a1
+    n = (steps - 65) // len(garden)
+    return b0 + b1 * n + (n * (n - 1) // 2) * (b2 - b1)
 
 
 if __name__ == "__main__":
